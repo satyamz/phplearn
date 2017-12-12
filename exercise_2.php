3ecb6914bf4a0215af33355f47ab36368e71a0bb
Satyam Zode <satyam.zode@cloudbyte.com>
2017-12-12 18:40:26 +0530

    Add solution for exercise 1 & 2

---
+++ exercise_2.php
@@ -0,0 +1,29 @@
+<?php
+
+// var_dump($argv);   //Shows all the command line args in $argv array.
+
+
+// Solution using foreach loop
+/*
+$sum = 0;
+$count = 0;
+foreach($argv as $arg) {
+    if ( $count != 0) {
+        $sum = $sum + $arg;        
+        $count++;
+    }
+    $count++;
+}
+
+echo $sum;
+*/
+
+
+//Solution using for loop
+
+$sum = 0;
+for($i = 1; $i < count($argv); $i++) {
+    $sum = $sum + $argv[$i];
+}
+
+echo $sum;
